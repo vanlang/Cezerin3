6ab20cd1adf79b6a2d89ba1bfc0cdf6bebf39c79
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-11-08 02:34:54 +0530

    Updating apps

--- src/admin/client/apps/jivosite.tsx
+++ src/admin/client/apps/jivosite.tsx
@@ -1,47 +1,30 @@
-import React from "react"
-import messages from "lib/text"
-import api from "lib/api"
-import TextField from "material-ui/TextField"
-import RaisedButton from "material-ui/RaisedButton"
+import { Button, TextField } from "@material-ui/core"
+import React, { useEffect, useState } from "react"
+import { api, messages } from "../lib"
 
 export const Description = {
   key: "jivosite",
   name: "JivoSite онлайн-консультант",
-  coverUrl: "/admin-assets/images/apps/jivosite.png",
+  coverUrl: "/admin-assets/images/apps/jivosite.webp",
   description: `JivoSite – чат для сайта и инструмент для общения с клиентами в социальных сетях, мессенджерах и мобильных приложениях. Зарабатывайте больше, не упуская ни одного обращения.`,
 }
 
-export class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      code: "",
+export const App = () => {
+  const [code, setCode] = useState("")
+
+  const fetchSettings = async () => {
+    try {
+      const { json } = await api.apps.settings.retrieve("jivosite")
+      const appSettings = json
+      if (appSettings) {
+        setCode(appSettings.code)
+      }
+    } catch (error) {
+      console.error(error)
     }
   }
 
-  handleChange = event => {
-    this.setState({
-      code: event.target.value,
-    })
-  }
-
-  fetchSettings = () => {
-    api.apps.settings
-      .retrieve("jivosite")
-      .then(({ status, json }) => {
-        const appSettings = json
-        if (appSettings) {
-          this.setState({ code: appSettings.code })
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  updateSettings = () => {
-    const { code } = this.state
-
+  const updateSettings = () => {
     api.apps.settings.update("jivosite", { code: code })
     api.theme.placeholders.update("jivosite", {
       place: "body_end",
@@ -49,35 +32,30 @@ export class App extends React.Component {
     })
   }
 
-  componentDidMount() {
-    this.fetchSettings()
-  }
+  useEffect(() => {
+    fetchSettings()
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <div>Введите код JivoSite</div>
-
-        <TextField
-          type="text"
-          multiLine={true}
-          fullWidth={true}
-          rows={10}
-          value={this.state.code}
-          onChange={this.handleChange}
-          floatingLabelText="Код чата JivoSite"
-          hintText="<!-- BEGIN JIVOSITE CODE {literal} -->..."
-        />
-
-        <div style={{ textAlign: "right" }}>
-          <RaisedButton
-            label={messages.save}
-            primary={true}
-            disabled={false}
-            onClick={this.updateSettings}
-          />
-        </div>
+  return (
+    <>
+      Введите код JivoSite
+      <TextField
+        type="text"
+        variant="outlined"
+        multiline
+        fullWidth
+        rows={10}
+        value={code}
+        onChange={event => setCode(event.target.value)}
+        label="Код чата JivoSite"
+        helperText="<!-- BEGIN JIVOSITE CODE {literal} -->"
+        style={{ marginTop: "25px" }}
+      />
+      <div style={{ textAlign: "right" }}>
+        <Button variant="contained" color="secondary" onClick={updateSettings}>
+          {messages.save}
+        </Button>
       </div>
-    )
-  }
+    </>
+  )
 }
