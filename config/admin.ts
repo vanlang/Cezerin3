3ca08ff2d7bb5d118134cd78c655330fc867fa0b
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-11-18 02:13:43 +0530

    🦺Fixing JWT🧵

--- config/admin.ts
+++ config/admin.ts
@@ -1,7 +1,7 @@
 // config used by dashboard client side only
 const Config = {
   // dashboard UI language
-  language: process.env.CONFIG_ADMIN_LANGUAGE || "en", // Edit at .env file!
+  language: "en",
   apiBaseUrl: "http://localhost:3001/api/v1",
   apiWebSocketUrl: "ws://localhost:3001",
   developerMode: true,
